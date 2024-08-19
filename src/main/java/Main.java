0b693e3ca6ba9b639bb0851e16561d9e611f090f
t20kdc <asdd2808@gmail.com>
2024-08-19 10:43:11 +0100

    make Maven a little more cooperative

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This is a dummy class intended to make Maven decide this project is worth downloading dependencies");
+	}
+}
